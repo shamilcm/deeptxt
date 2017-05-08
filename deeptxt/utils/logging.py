6d4b1e5e77e01cfe463f65f1e4ac599107147cf8
Shamil Chollampatt <shamil.cm@gmail.com>
2017-05-08 14:38:10 +0800

    logging during training and validation

---
+++ deeptxt/utils/logging.py
@@ -0,0 +1,32 @@
+import sys
+
+def train_info(epoch, loss, num_batches=None, updates=None,  time_per_update=None):
+    print >> sys.stderr, 'Epoch: ', epoch,
+    if num_batches:
+        print >> sys.stderr, 'Batches: ', train_batches,
+    if updates:
+        print >> sys.stderr, 'Updates: ', updates,
+    
+    print >> sys.stderr, 'Avg.Loss/Batch: ', "{0:.4f}".format(train_loss_sum/train_batches),
+    
+    if time_per_update:
+        print >> sys.stderr, 'Duration/Update:', time_per_update
+    else:
+        print >> sys.stderr, ''
+
+def validation_info(validator, print_loss=True, print_metric=False, run_script=False):
+    print >> sys.stderr, ''
+    print >> sys.stderr, 'Validation...'
+    
+    if print_loss:
+        validation_loss = validator.compute_loss()
+        print >> sys.stderr, '\tLoss:', "{0:.4f}".format(validation_loss),
+    if print_metric:
+        validation_score = validator.compute_metric()
+        if validation_score:
+            print >> sys.stderr, validator.metric.metric_name, "{0:.4f}".format(validation_score),
+    else:
+        print >> sys.stderr, ''
+    print >> sys.stderr, ''
+    if run_script:
+        validator.run_external_script()
