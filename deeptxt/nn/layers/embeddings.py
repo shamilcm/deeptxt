9d17a5d81a05d431de47da2b6a76dc9104b04165
Shamil Chollampatt <shamil.cm@gmail.com>
2017-05-09 15:18:15 +0800

    add bos token in embeddings

--- deeptxt/nn/layers/embeddings.py
+++ deeptxt/nn/layers/embeddings.py
@@ -18,15 +18,22 @@ class Embeddings(Layer):
         vocab_size: size of the input = size of vocabulary
         emb_dim: word embeddings dimension
         initializer: method to initialize the embeddings
+        allow_bos: allow a 0-vector for the bos token
     """    
-    def __init__(self, name, vocab_size, emb_dim, initializer=Initializer.norm):
+    def __init__(self, name, vocab_size, emb_dim, initializer=Initializer.norm, add_bos=False):
+        if add_bos == True:
+            vocab_size = vocab_size + 1
+        self.add_bos = add_bos
         self.in_dim = vocab_size
         self.out_dim = emb_dim
         self.initialize_params(name, vocab_size, emb_dim, initializer)
         
     def initialize_params(self, name, vocab_size, emb_dim, initializer=Initializer.norm):
         self._params = OrderedDict()
-        self.Emb = theano.shared(name=name, value=initializer(size=(vocab_size, emb_dim)))
+        embeddings = initializer(size=(vocab_size, emb_dim))
+        if self.add_bos:
+            embeddings[-1] = 0.
+        self.Emb = theano.shared(name=name, value=embeddings)
         self._params[self.Emb.name] = self.Emb
     
     def params(self):
