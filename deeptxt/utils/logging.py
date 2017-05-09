40e871791508160f6db8b2d595f737144a833180
Shamil Chollampatt <shamil.cm@gmail.com>
2017-05-09 15:17:45 +0800

    wrong variables used

--- deeptxt/utils/logging.py
+++ deeptxt/utils/logging.py
@@ -1,13 +1,14 @@
 import sys
 
+
 def train_info(epoch, loss, num_batches=None, updates=None,  time_per_update=None):
     print >> sys.stderr, 'Epoch: ', epoch,
     if num_batches:
-        print >> sys.stderr, 'Batches: ', train_batches,
+        print >> sys.stderr, 'Batches: ', num_batches,
     if updates:
         print >> sys.stderr, 'Updates: ', updates,
     
-    print >> sys.stderr, 'Avg.Loss/Batch: ', "{0:.4f}".format(train_loss_sum/train_batches),
+    print >> sys.stderr, 'Avg.Loss/Batch: ', "{0:.4f}".format(loss/num_batches),
     
     if time_per_update:
         print >> sys.stderr, 'Duration/Update:', time_per_update
