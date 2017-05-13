01c2e3743432ba03ffd8e963b5e7e65a4c540b3a
Shamil Chollampatt <shamil.cm@gmail.com>
2017-05-14 00:02:07 +0800

    added sampling, and bos token

--- deeptxt/models/rnn_unidirectional_encoder_decoder.py
+++ deeptxt/models/rnn_unidirectional_encoder_decoder.py
@@ -9,20 +9,25 @@ from collections import OrderedDict
 
 from .model import Model
 from ..nn.layers.embeddings import Embeddings
-from ..nn.layers.gru import GRU, ConditionalGRU
 from ..nn.layers.dense import Dense
 from ..nn.activations import Activation
 
 
 class RNNUnidirectionalEncDec(Model):
-    
+
     def __init__(self, hyperparams, encoder_vocab, decoder_vocab):
         self.hyperparams = hyperparams
         self.encoder_vocab = encoder_vocab
         self.decoder_vocab = decoder_vocab
+
+        # hyperparams.encoder_vocab_size and hyperparams.decoder_vocab_size setting to max
+        # TODO: Uncomment this and throw error
+        #hyperparams.encoder_vocab_size = min(hyperparams.encoder_vocab_size, encoder_vocab.vocab_size)
+        #hyperparams.decoder_vocab_size = min(hyperparams.decoder_vocab_size, decoder_vocab.vocab_size)
+
         # Preparing and Initializing Network Weights & Biases
         self.setup()
-    
+
 
     # TODO: Loading and storing params
     def setup(self):
@@ -40,29 +45,39 @@ class RNNUnidirectionalEncDec(Model):
         self._params.update(self.decoder_embeddings.params())
         
         
+        ################
         # Encoder Layer
+        ################
+        # TODO: make a different class
         if self.hyperparams.rnn_cell == 'gru':
-            # left-to-right rnn
-            self.encoder_rnn_layer_l2r = GRU(name='encoder_gru0_l2r', in_dim=self.hyperparams.encoder_emb_dim, num_units=self.hyperparams.encoder_units)
-            self._params.update(self.encoder_rnn_layer_l2r.params())
+            from ..nn.layers.gru import GRU as RNN
         elif self.hyperparams.rnn_cell == 'lstm':
             raise NotImplementedError
         else:
             logger.error("Invalid RNN Cell Type:" + self.hyperparams.rnn_cell)
+
+        self.encoder_rnn_layer_l2r = RNN(name='encoder' + self.hyperparams.rnn_cell + '0_l2r', in_dim=self.hyperparams.encoder_emb_dim, num_units=self.hyperparams.encoder_units)
+        self._params.update(self.encoder_rnn_layer_l2r.params())
+
         
         # Transform to prepare init state of decoder
         self.decoder_init_transform = Dense(name='decoder_init_transform', in_dim=self.hyperparams.encoder_units, num_units=self.hyperparams.decoder_units, activation=Activation.tanh)
         self._params.update(self.decoder_init_transform.params())
         
+        ################
         # Decoder Layer
+        ###############
+        # TODO: make a different class
         if self.hyperparams.rnn_cell == 'gru':
-            self.decoder_rnn_layer = ConditionalGRU(name='decoder_gru0', in_dim=self.hyperparams.decoder_emb_dim, num_units=self.hyperparams.decoder_units, context_dim=self.hyperparams.encoder_units)
-            self._params.update(self.decoder_rnn_layer.params())
+            from ..nn.layers.gru import ConditionalGRU as ConditionalRNN
         elif self.hyperparams.rnn_cell == 'lstm':
             raise NotImplementedError
         else:
             logger.error("Invalid RNN Cell Type:" + self.hyperparams.rnn_cell)
-            
+
+        self.decoder_rnn_layer = ConditionalRNN(name='decoder_' + self.hyperparams.rnn_cell + '0', in_dim=self.hyperparams.decoder_emb_dim, num_units=self.hyperparams.decoder_units, context_dim=self.hyperparams.encoder_units)
+        self._params.update(self.decoder_rnn_layer.params())
+
         # Read out words
         
         self.decoder_state_transform = Dense(name='decoder_state_transform', in_dim=self.hyperparams.decoder_units, num_units=self.hyperparams.decoder_emb_dim, activation=Activation.linear)
@@ -99,21 +114,19 @@ class RNNUnidirectionalEncDec(Model):
         enc_emb = enc_emb.reshape([self.x.shape[0], self.x.shape[1], self.hyperparams.encoder_emb_dim])
         
         # get decoder init state
-        encoder_outputs = self.encoder_rnn_layer_l2r.build(enc_emb,  self.x_mask)[0] 
-        last_encoder_output = encoder_outputs[-1]  # This will be the context at every input step
+        self.encoder_outputs = self.encoder_rnn_layer_l2r.build(enc_emb,  self.x_mask)[0]
+        last_encoder_output = self.encoder_outputs[-1]  # This will be the context at every input step
         
         # transform encoder output to get decoder init
-        decoder_init = self.decoder_init_transform.build(last_encoder_output)
+        self.decoder_init = self.decoder_init_transform.build(last_encoder_output)
         
         # input
         # TODO: remove embedding shifting?
         dec_emb = self.decoder_embeddings.Emb[self.y.flatten()]
         dec_emb = dec_emb.reshape([self.y.shape[0], self.y.shape[1], self.hyperparams.decoder_emb_dim])
-        dec_emb_shifted = T.zeros_like(dec_emb)
-        dec_emb_shifted = T.set_subtensor(dec_emb_shifted[1:], dec_emb[:-1])
         
         # Building the RNN layer
-        decoder_outputs = self.decoder_rnn_layer.build(x=dec_emb_shifted, x_mask=self.y_mask, c=last_encoder_output, h_init=decoder_init)[0]  # Only one output, hidden states at every time step
+        self.decoder_outputs = self.decoder_rnn_layer.build(x=dec_emb, x_mask=self.y_mask, c=last_encoder_output, h_init=self.decoder_init)[0]  # Only one output, hidden states at every time step
         
         # context with new axis. condition the output on encoder context as well.
         # TODO: remove axis adding?
@@ -122,49 +135,114 @@ class RNNUnidirectionalEncDec(Model):
         # dim(proj_h) = #timesteps x #samples x #num_units
         
         # Computing word probabilities
-        logit_decoder_rnn = self.decoder_state_transform.build(decoder_outputs)  # dim(logit_rnn) = #timesteps x #samples x #emb_dim
-        logit_prev_emb = self.prev_emb_transform.build(dec_emb_shifted)   # dim(logit_prev) = #timesteps x #samples x #emb_dim
+        logit_decoder_rnn = self.decoder_state_transform.build(self.decoder_outputs)  # dim(logit_rnn) = #timesteps x #samples x #emb_dim
+        logit_prev_emb = self.prev_emb_transform.build(dec_emb)   # dim(logit_prev) = #timesteps x #samples x #emb_dim
         logit_enc_context = self.encoder_context_transform.build(context)
         logit = self.word_probs_transform.build(Activation.tanh(logit_decoder_rnn + logit_prev_emb + logit_enc_context)) # dim(logit) = #timesteps x #samples x #vocab_size
         
         # reshaping logit as (#timesteps*#samples) x vocab_size and performing softmax across vocabulary
         self.probs = T.nnet.softmax(logit.reshape([logit.shape[0]*logit.shape[1], logit.shape[2]])) #dim(probs) = (#timesteps*#samples) x vocab_size
+        self.debug = [self.probs.shape, self.y.shape, self.y_mask.shape]
         #Building loss function
         self.build_loss()
-        
+
+
         self._outputs = [self.probs]
 
     def build_loss(self):
         # TODO: Make it better?
-        y_flat = self.y.flatten() #x_flat: a linear array with size #timesteps*#samples
+        # y[0]  is bos, remove it to calculate loss
+        y_flat = self.y[1:].flatten() #x_flat: a linear array with size #timesteps*#samples
         y_flat_idx = T.arange(y_flat.shape[0]) * self.hyperparams.decoder_vocab_size + y_flat
-                                    
+
         self._loss = -T.log(self.probs.flatten()[y_flat_idx])
-        self._loss = self._loss.reshape([self.y.shape[0], self.y.shape[1]])
-        self._loss = (self._loss * self.y_mask).sum(0)    
+        self._loss = self._loss.reshape([self.y.shape[0]-1, self.y.shape[1]])
+        self._loss = (self._loss * self.y_mask[1:]).sum(0)
+
+    def build_sampler(self, sampling=True):
+        initializer_input = [self.x, self.x_mask]
+        initializer_output = [self.encoder_outputs, self.decoder_init]
+        self.initializer = theano.function(initializer_input, initializer_output)
+
+        sampler_input = [self.y, self.y_mask] + initializer_output
+
+        if sampling == True:
+            # sample a word from the output softmax, instead of selecting argmax
+            next_token_index = self.trng.multinomial(pvals=self.probs).argmax(1)  # multinomial will represent 1 hot representation of the selected sample
+        else:
+            next_token_index = T.argmax(self.probs, axis=1)
+
+        sampler_output = [self.probs, next_token_index, self.decoder_outputs]
+        self.sampler = theano.function(sampler_input, sampler_output)
+
+    def sample(self, batch, num_samples=5):
+        source, source_mask, target, target_mask = self.prepare_input(batch)
+        num_samples = np.minimum(1, source.shape[1])
+        # TODO: replace by random sampling:
+        source = source[:,0:num_samples]
+        source_mask = source_mask[:,0:num_samples]
+        target = target[:, 0:num_samples]
+        target_mask = target_mask[:, 0:num_samples]
+
+        samples = []
+        for sample_index in xrange(num_samples):
+            hypothesis = self.encode_decode([ source[:, sample_index:sample_index+1], source_mask[:, sample_index:
+                sample_index+1] ])
+            hypothesis_sent = ' '.join(hypothesis)
+
+            source_sent = ' '.join([self.encoder_vocab.get_token(index) for index in source[:, sample_index] if index != self.encoder_vocab.get_index(self.encoder_vocab.eos)])
+
+            #hypothesis_sent = ' '.join([self.decoder_vocab.get_token(index) for index in target[:, sample_index] if index != self.decoder_vocab.get_index(self.decoder_vocab.eos)])
+
+            target_sent = ' '.join([self.decoder_vocab.get_token(index) for index in target[:, sample_index] if index != -1 and index != self.decoder_vocab.get_index(self.decoder_vocab.eos)])
+            # TODO: change sample to dictionary, and in trainer display all keys and values
+            samples.append(OrderedDict({'SRC': source_sent, 'HYP': hypothesis_sent, 'REF': target_sent}))
+
+        return samples
+
+    def encode_decode(self, test_input, max_length=50):
+        encoding = self.initializer(*test_input)
+        init_input = [np.array([[-1]]), np.array([[1.]], dtype='float32')] + encoding
+
+        probs, next_token_index, decoder_outputs = self.sampler(*init_input)
+
+        hypothesis = []
+        hyp_length = 0
+        while(next_token_index[0] != self.decoder_vocab.get_index(self.decoder_vocab.eos)):
+            hypothesis.append(self.decoder_vocab.get_token(next_token_index[0]))
+            hyp_length += 1
+            # This is if next_token_index is a scalar, i.e. when only one column is passed as test_input
+            # [None] adds a new axis
+            next_input = [next_token_index[None], np.array([[1.]], dtype='float32')] + encoding
+            probs, next_token_index, decoder_outputs = self.sampler(*next_input)
+            if hyp_length == max_length:
+                break
+
+        return hypothesis
 
     def loss(self):
         return self._loss.mean()
-    
+
     def log_probs(self):
         # TODO: Make it better?
         return self._loss
-    
+
     def outputs(self):
         return self._outputs
-    
+
     def inputs(self):
         return [self.x, self.x_mask, self.y, self.y_mask]
-        
+
     def params(self):
         return self._params
-    
+
+
 
     def prepare_input(self, batch, max_length=None):
         # setting maxlen to length of longest sample
         max_length_input = max([len(sample[0]) for sample in batch]) 
         max_length_target = max([len(sample[1]) for sample in batch])
-        
+
         # adding end of sentence marker
         inp = [[self.encoder_vocab.get_index(token) for token in sample[0]] + [self.encoder_vocab.get_index(self.encoder_vocab.eos)] 
                for sample in batch]
@@ -172,12 +250,13 @@ class RNNUnidirectionalEncDec(Model):
                for sample in batch]
         max_length_input += 1
         max_length_target += 1
-        
+
         # preparing mask and input
-        inp_mask = np.array([[1.]*len(inp_instance[:max_length_input]) + [0.]*(max_length_input-len(inp_instance)) for inp_instance in inp], dtype='float32').transpose()  
-        inp = np.array(               [inp_instance[:max_length_input]  + [0.]*(max_length_input-len(inp_instance)) for inp_instance in inp], dtype='int64').transpose()
-        target_mask = np.array([[1.]*len(target_instance[:max_length_target]) + [0.]*(max_length_target-len(target_instance)) for target_instance in target], dtype='float32').transpose()  
-        target = np.array(               [target_instance[:max_length_target]  + [0.]*(max_length_target-len(target_instance)) for target_instance in target], dtype='int64').transpose()
-       
-        return inp, inp_mask, target, target_mask
+        source_mask = np.array([[1.]*len(inp_instance[:max_length_input]) + [0.]*(max_length_input-len(inp_instance)) for inp_instance in inp], dtype='float32').transpose()
+        source = np.array(               [inp_instance[:max_length_input]  + [0.]*(max_length_input-len(inp_instance)) for inp_instance in inp], dtype='int64').transpose()
+
+        # taret preparation with -1 (beginning of sentence) row upfront
+        target_mask = np.array([[1.] + [1.]*len(target_instance[:max_length_target]) + [0.]*(max_length_target-len(target_instance)) for target_instance in target], dtype='float32').transpose()
+        target = np.array([[-1] + target_instance[:max_length_target]  + [0.]*(max_length_target-len(target_instance)) for target_instance in target], dtype='int64').transpose()
+        return source, source_mask, target, target_mask
 
