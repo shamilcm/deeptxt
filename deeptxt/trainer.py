9c3531db0e28280f205baf678984cbbe79c73c89
Shamil Chollampatt <shamil.cm@gmail.com>
2017-05-25 15:46:12 +0800

    loading data in trainer

--- deeptxt/trainer.py
+++ deeptxt/trainer.py
@@ -1,6 +1,7 @@
 import theano
 import numpy as np
 import logging
+import time
 
 
 from .utils import logutils
@@ -8,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 class Trainer:
 
-    def __init__(self, model, optimizer, loss=None):
+    def __init__(self, model, optimizer, loss=None, cache_minibatches=False):
         """
         """
         self._model = model
@@ -19,29 +20,58 @@ class Trainer:
 
         self.num_updates = 0
 
+        self.cache_minibatches = cache_minibatches
+        self.input_cache = dict()
+
+
         self.f_debug = None
         if hasattr(model, 'debug') and model.debug and None not in model.debug:
             self.f_debug = theano.function(model.inputs(), model.debug, on_unused_input='ignore')
 
+        # for calculating time and speed
+        self.num_tokens_seen = 0
+        self.num_samples_seen = 0
+        self.last_log_updates = 0
+        self.last_log_time = time.time()
+
+
+
     def loss():
         return loss
 
-    def update(self, minibatch, max_length=None, verbose=False):
-        inp = self._model.prepare_train_input(minibatch, max_length)
-        #print "DEBUG, shape:", [i.shape for i in inp]
-        if inp is None:
+    def update(self, data_reader,  max_length=None, verbose=False, minibatch_index=None):
+
+        if minibatch_index and self.cache_minibatches == True and minibatch_index in self.input_cache:
+            self.current_input = self.input_cache[minibatch_index]
+        else:
+            minibatch = data_reader.next()
+            # reached end of epoch, return None as loss
+            if minibatch is None:
+                return None
+            self.current_input = self._model.prepare_train_input(minibatch, max_length)
+
+            if self.cache_minibatches == True:
+                self.input_cache[minibatch_index] = self.current_input
+
+        # For caclulating speed
+        self.num_tokens_seen += np.sum(self.current_input[-1])
+        self.num_samples_seen += self.current_input[-1].shape[1]
+
+        #print "DEBUG, shape:", [i.shape for i in self.current_input]
+        if self.current_input is None:
             return None
 
         if verbose == True and self.f_debug:
-            logger.debug('Debug Message:' + str(self.f_debug(*inp)))
+            logger.debug('Debug Message:' + str(self.f_debug(*self.current_input)))
 
-        train_loss = self.forward(*inp) #Calculate loss and gradient
+        train_loss = self.forward(*self.current_input) #Calculate loss and gradient
         self.backward()  # Update prameters
+
         self.num_updates += 1
         return train_loss
 
-    def sampling(self, minibatch, num_samples=5):
-        samples = self._model.sample(minibatch, num_samples)   # sample is a tuple of (source, output, reference)
+    def sampling(self,  num_samples=5):
+        samples = self._model.sample(self.current_input, num_samples)   # sample is a tuple of (source, output, reference)
         for i in xrange(len(samples)):
             logger.info('Sample %d' %i)
             for label, value in samples[i].iteritems():
@@ -66,14 +96,25 @@ class Trainer:
         self.num_updates =  loaded_arrs['num_updates']
 
 
-    def log_train_info(self, epoch, loss, num_batches=None,  time_per_update=None, speed=None):
+    def log_train_info(self, epoch, loss, num_batches=None):
+
+        # calculating duration and time per update
+        log_duration = time.time() - self.last_log_time
+        time_per_update = log_duration / (self.num_updates - self.last_log_updates)
+
         log_msg = 'Epoch#: %d'  % epoch
         if num_batches:
             log_msg += '\t| Minibatch#: %d' % num_batches
         log_msg += '\t| Update#: %d' % self.num_updates
         log_msg += '\t| Avg. Loss: ' + "{0:.4f}".format(loss/num_batches)
 
-        if speed:
-            log_msg +=  '\t| Speed: ' + ' '.join(speed)
+        log_msg += '\t| Time/Update: ' + "{0:.2f}".format(time_per_update) + ' s'
+        log_msg +=  '\t| Speed: ' + str(int(self.num_tokens_seen * 1.0 / log_duration)) + ' words/s , ' +  str(int(self.num_samples_seen * 1.0 / log_duration)) + ' samples/s '
 
+        self.num_tokens_seen = 0
+        self.num_samples_seen = 0
         logger.info(log_msg)
+
+        # resetting timer and setting last updates
+        self.last_log_time = time.time()
+        self.last_log_updates = self.num_updates
