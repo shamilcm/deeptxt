c3c5b51bda8728342989813f115e959961336050
Shamil Chollampatt <shamil.cm@gmail.com>
2017-05-31 16:54:21 +0800

    verbose Theano

--- deeptxt/utils/theano_config.py
+++ deeptxt/utils/theano_config.py
@@ -1,11 +1,12 @@
-def configure(device='cpu', dtype='float32'):
+def configure(device='cpu', dtype='float32', verbose=False):
     import numpy as np
     np.random.seed(1234)
     import os
     flags = "device=" + device 
     flags += ',floatX=' + dtype
     flags += ',warn_float64=warn' 
-    #flags += ',exception_verbosity=high'
+    if verbose == True:
+        flags += ',exception_verbosity=high'
     #flags += ',optimizer=fast_compile'
     os.environ["THEANO_FLAGS"]  = flags
     import theano
